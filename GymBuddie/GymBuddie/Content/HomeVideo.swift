b049e0b4e185c3330c266e806075da0c626c9388
Jake-Len <60149351+Jake-Len@users.noreply.github.com>
2020-02-19 20:26:43 -0500

    Add files via upload

---
+++ GymBuddie/GymBuddie/Content/HomeVideo.swift
@@ -0,0 +1,21 @@
+//
+//  HomeVideo.swift
+//  GymBuddie
+//
+//  Created by Jacob Leonard on 2/8/20.
+//  Copyright © 2020 Jacob Leonard. All rights reserved.
+//
+
+import SwiftUI
+
+struct HomeVideo: View {
+    var body: some View {
+        
+    }
+}
+
+struct HomeVideo_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeVideo()
+    }
+}
---
+++ GymBuddie/GymBuddie/Content/VideoControl.swift
@@ -0,0 +1,22 @@
+//
+//  VideoControl.swift
+//  GymBuddie
+//
+//  Created by Jacob Leonard on 2/10/20.
+//  Copyright © 2020 Jacob Leonard. All rights reserved.
+//
+
+import SwiftUI
+import AVKit
+
+struct VideoControl: View {
+    var body: some View {
+        Text("Hi")
+    }
+}
+
+struct VideoControl_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoControl()
+    }
+}
